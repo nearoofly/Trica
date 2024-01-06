a4c4d868c37b6a7fcd73986ced8c7b8d06609770
Goofly <130136825+nearoofly@users.noreply.github.com>
2024-01-06 23:30:29 +0300

    Update trica.py

--- trica.py
+++ trica.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Page d'accueil demandant le consentement
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Page pour afficher l'écran une fois le consentement donné
+
 @app.route('/ecran')
 def ecran():
     return render_template('ecran.html')
 
-# Route pour gérer le consentement de l'utilisateur
+
 @app.route('/autoriser', methods=['POST'])
 def autoriser():
-    # Traitement du consentement, enregistrement dans la base de données, etc.
+    
     consentement_accorde = True
 
     if consentement_accorde:
